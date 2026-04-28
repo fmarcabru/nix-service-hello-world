40554bd78bc881901f453956e97552617599f7c9
fmarcabru <onin@pm.me>
2026-04-28 23:09:08 +1000

    package with nix

--- flake.nix
+++ flake.nix
@@ -24,10 +24,45 @@
 
     outputs = { self, nixpkgs, uv2nix, pyproject-nix, pyproject-build-systems, ... }:
         let
+            inherit (nixpkgs) lib;
             system = "x86_64-linux";
             pkgs = nixpkgs.legacyPackages.${system};
+            python = pkgs.python312;
+
+            # read pyproject.toml + uv.loc from the repo
+            workspace = uv2nix.lib.workspace.loadWorkspace {
+                workspaceRoot = ./.;
+            };
+
+            # Translate uv.lock in to a nix overlay
+            overlay = workspace.mkPyprojectOverlay {
+                sourcePreference = "wheel";
+            };
+
+            # Build the full python package set with the overlay applied
+            pythonSet = (pkgs.callPackage pyproject-nix.build.packages {
+                inherit python;
+                }).overrideScope (
+                    lib.composeManyExtensions [
+                        pyproject-build-systems.overlays.default
+                        overlay
+                    ]
+                );
+            # the virtual env wih app and dependencies
+            venv = pythonSet.mkVirtualEnv "fastapi-hello-env" workspace.deps.default;
+
         in
         {
+            # with with nix run
+            packages.${system}.default = pkgs.writeShellApplication {
+                name = "fastapi-hello";
+                runtimeInputs = [ venv ];
+                text = ''
+                    exec uvicorn fastapi_hello.main:app --host 0.0.0.0 --port 8080
+                '';
+            };
+
+            # dev shell for nix develop
             devShells.${system}.default = pkgs.mkShell {
                 packages = [
                     pkgs.uv
